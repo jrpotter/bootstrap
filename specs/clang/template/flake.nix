4a0f1aa9195477cea1033a248d3998d08224e464
Joshua Potter <jrpotter2112@gmail.com>
2023-12-19 15:36:21 -0700

    clang: Migrate to `CMake`, dropping `bear`.

--- specs/clang/template/flake.nix
+++ specs/clang/template/flake.nix
@@ -15,7 +15,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -30,8 +30,8 @@
           stdenv = pkgs.clangStdenv;
         } {
           packages = with pkgs; [
-            bear
             clang-tools
+            cmake
             codelldb
             doxygen
           ];
